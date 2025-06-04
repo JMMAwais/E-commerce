24693933fde63044543eccab5e81903f332528fe
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-06-04 11:17:01 -0700

    change1

--- E-commerce/Program.cs
+++ E-commerce/Program.cs
@@ -10,6 +10,7 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 builder.Services.AddAuthentication(opt => {
     opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
